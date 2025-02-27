f3531c05c1c339b2259a3b8c6d8adbd232ab1ef8
alvarordn <alvarordn@hotmail.com>
2025-02-27 08:31:36 +0100

    Incluida figuras de un ataque

--- state_estimator/reading_and_plotting.py
+++ state_estimator/reading_and_plotting.py
@@ -2,91 +2,12 @@ import numpy as np
 import json
 import matplotlib.pyplot as plt
 from pydae import plot_tools
-from collections import Counter
+from lib_timeseries import system_topology, system_measurements
 
 # Leemos el json de resultados
-with open('data_simus_ts.json','r') as file:
+with open('data_simus_ts_1ataque.json','r') as file:
     data = json.load(file)     
     
-# '090neg', '090pos', '100pos'
-# '08', '09', '10', '11', '12', '13', '14'
-# 'P', 'Q', 'U'
-
-# Recordamos los valores de lambda considerados
-num_lmb = 100
-lmb_range = np.linspace(0.01, 50, num_lmb)
-
-# Generamos los ataques para cada variable
-c = '090neg'
-hour = '14'
-x_values = [str(item) for item in np.linspace(0.01, 50, 100)]
-subplots_titles = ['P', 'Q', 'U']
-
-fig, axs = plt.subplots(3, 1, figsize=(8, 6), sharex=True)
-
-# Definimos los colores del plot
-colors = plot_tools.set_style(plt)
-# colors.pop(3)
-
-# Establecemos las etiquetas
-labels = ['Detection', 'Detection and others', 'Incorrect detection', 'No detection']
-
-# Inicializamos figura
-for i, pqu in enumerate(subplots_titles):
-    counts = []
-
-    for x in x_values:
-        values = list(data[c][hour][x][pqu].values())
-        counts.append(values)
-
-    counts = np.array(counts).T  # Transponer para separar por 0, 1, 2, 3
-
-    axs[i].stackplot(
-        lmb_range, 
-        counts, 
-        labels=labels,
-        alpha=0.7
-    )
-
-    axs[i].set_ylabel(subplots_titles[i])
-    axs[i].grid(True)
-
-axs[-1].legend()
-axs[-1].set_xlabel('$\lambda$')
-plt.tight_layout()
-plt.show()
-
-
-
-
-#%%
-
-from lib_timeseries import system_topology, system_measurements, system_constraints
-import lib
-import time
-
-P_gen = list() 
-
-minute = '00'
-path = ['../data_Cati2/pv_2_3_180_', '../data_Cati2/pv_2_3_180_']
-for c in ['090neg']:        
-    for hour in ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23']:        
-            
-        sheet_name = hour + '_' + minute + '_pf_' + c
-        extended_path = [path[0] + hour + '_' + minute + '_pf_' + c + '/',
-                         path[1] + hour + '_' + minute + '_pf_' + c + '/']
-
-        Nodes, Lines = system_topology('../data/pv_2_3.json')
-        Meas, mjson, stdjson = system_measurements(extended_path, 
-                                                   'measurements.json', 
-                                                   'std_2.json', 
-                                                   Nodes, 
-                                                   Lines, 
-                                                   add_noise = True)
-        P_gen.append(Meas[0]['value'])
-plt.plot(P_gen)
-
-#%%
 
 tipo = 'U'
 
@@ -134,7 +55,7 @@ axs[0,0].stackplot(
 )
 axs[0,0].grid(True)
 for it in range(8, 15):
-    axs[0,0].plot([it, it],[0,0.35],color='k', alpha=0.5)
+    axs[0,0].plot([it, it],[0,0.35], 'k-.', alpha=0.5)
 axs[0,0].set_ylim([0.0, 0.4])
 
 labels_time = ['8h', '9h', '10h', '11h', '12h', '13h', '14h']
@@ -205,9 +126,16 @@ for c in ['090neg']:
         if i != 0 or j != 0:
             axs[i,j].set_ylabel(labels_time[j*4+i-1])
             axs[i,j].set_xlim([0,50])
+            axs[i,j].set_ylim([0,100])
         axs[i,j].grid(True)
-        
-    # axs[-1].legend()
-    # axs[-1].set_xlabel('$\lambda$')
-    # plt.tight_layout()
-    # plt.show()
+    
+    
+lines = axs[0, 1].collections
+legend=fig.legend(lines, labels, loc='upper center', ncol=4, bbox_to_anchor=(0.674, 0.93))
+# fig.text(0.18, 0.901, 'Single attack - ' + tipo, fontsize=12)
+legend_edge_color = legend.get_frame().get_edgecolor()
+
+fig.text(0.18, 0.901, 'Single attack - ' + tipo, fontsize=12,
+         bbox=dict(facecolor='white', edgecolor=legend_edge_color, boxstyle='round,pad=0.3'))
+
+
