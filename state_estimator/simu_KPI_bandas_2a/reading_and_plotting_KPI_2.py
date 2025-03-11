a82b1287c0877efdd00138bb434d4070bb2ab51e
arnozal <arnozal@us.es>
2025-03-11 16:49:30 +0100

    Gráficas para ataques dobles

--- state_estimator/simu_KPI_bandas_2a/reading_and_plotting_KPI_2.py
+++ state_estimator/simu_KPI_bandas_2a/reading_and_plotting_KPI_2.py
@@ -1,16 +1,16 @@
 import numpy as np
 import json
 import matplotlib.pyplot as plt
-from pydae import plot_tools
-from lib_timeseries import system_topology, system_measurements
-import matplotlib.gridspec as gridspec
+from matplotlib.gridspec import GridSpec
 import os
 
+# Configuración inicial
 fp = '090neg'
 hour = '10'
-tipo = 'U'
+tipo = 'P'
 num_indices = 8 
-num_subplots = 6  
+metrics = ['Precision', 'Accuracy', 'Recall']  # Métricas a representar
+num_subplots = len(metrics)
 
 data_matrices = [np.zeros((num_indices, num_indices)) for _ in range(num_subplots)]
 
@@ -20,9 +20,9 @@ for i in range(num_indices):
         if os.path.exists(file_name):
             with open(file_name, 'r') as file:
                 data = json.load(file)
-                for k in range(num_subplots):
+                for k, metric in enumerate(metrics):
                     try:
-                        value = data[fp][hour]['2.5'][tipo][str(k)]
+                        value = data[fp][hour]['2.5'][tipo][metric] * 100
                         data_matrices[k][i, j] = value
                     except KeyError:
                         data_matrices[k][i, j] = np.nan  
@@ -30,13 +30,18 @@ for i in range(num_indices):
             for k in range(num_subplots):
                 data_matrices[k][i, j] = np.nan
 
-fig, axes = plt.subplots(2, 3, figsize=(18, 9), gridspec_kw={'wspace': -0.2})
+# Crear figura y rejilla con espacio para la barra de color
+fig = plt.figure(figsize=(18, 6))
+gs = GridSpec(1, 4, width_ratios=[1, 1, 1, 0.05], wspace=0.3)  # 3 subplots + espacio para colorbar
+
+axes = [fig.add_subplot(gs[0]), fig.add_subplot(gs[1]), fig.add_subplot(gs[2])]
+cax = fig.add_subplot(gs[3])  
+
 cmap = plt.cm.magma  
 norm = plt.Normalize(vmin=0, vmax=100)
 labels = ['80-85%', '85-90%', '90-95%', '95-100%', '100-105%', '105-110%', '110-115%', '115-120%']
 
-titulos = ['Detection', 'Detection and others', 'Partial detection', 'Partial detection and others', 'No detection', 'Incorrect detection']
-for k, ax in enumerate(axes.flat):
+for k, ax in enumerate(axes):
     grid = ax.imshow(data_matrices[k], cmap=cmap, norm=norm)
     
     for i in range(num_indices):
@@ -48,35 +53,12 @@ for k, ax in enumerate(axes.flat):
     ax.set_xticks(np.arange(num_indices))
     ax.set_yticks(np.arange(num_indices))
     ax.set_xticklabels(labels, rotation=45, ha="right")
-    ax.set_yticklabels(labels)
-    ax.set_title(titulos[k])
+    ax.set_yticklabels(labels if k == 0 else [])
+    ax.set_title(metrics[k])
 
+# Agregar la barra de color en el espacio reservado
+cbar = fig.colorbar(grid, cax=cax)
 
-axes[0,0].set_xticks([])
-axes[0,1].set_xticks([])
-axes[0,2].set_xticks([])
-axes[0,1].set_yticks([])
-axes[0,2].set_yticks([])
-axes[1,1].set_yticks([])
-axes[1,2].set_yticks([])
-
-
-fig.colorbar(grid, ax=axes.ravel().tolist())
-
-
-plt.savefig('figs/'+ file_name.split('.')[0] + '_' + tipo + '.pdf')
-plt.savefig('figs/'+ file_name.split('.')[0] + '_' + tipo + '.png')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+plt.savefig('figs/' + file_name.split('.')[0] + '_kpi_' + tipo + '.pdf')
+plt.savefig('figs/' + file_name.split('.')[0] + '_kpi_' + tipo + '.png')
+plt.show()
